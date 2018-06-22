48c24d0cc84d7dedbe8b08c3490e04fccb06edc2
KBSure <kbsure.dev@gmail.com>
2018-06-22 14:23:42 +0900

    리스트 1-4

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -8,15 +8,10 @@ public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        String url = "jdbc:mysql://localhost:3306/springbook?serverTimezone=UTC&useSSL=false";
-        String dbUser = "spring";
-        String password = "book";
-        Connection con = DriverManager.getConnection(url, dbUser, password);
+        Connection con = getConnection("jdbc:mysql://localhost:3306/springbook");
 
         String sql = "insert into users(id, name, password) values(?, ?, ?)";
-        PreparedStatement ps = con.prepareStatement(sql);
+        PreparedStatement ps = con.prepareStatement(sql); //벤더사가 구현  //PS는 ? 사용 해서 동적 sql 가능 //sqlInjection 공격에 안전
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
         ps.setString(3, user.getPassword());
@@ -27,13 +22,17 @@ public class UserDao {
         con.close();
     }
 
-    public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    private Connection getConnection(String s) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver"); //각 DB 만든 회사들은 다르다. 패키지명도 다 다를테니까 호환성
 
-        String url = "jdbc:mysql://localhost:3306/springbook?serverTimezone=UTC&useSSL=false";
+        String url = s;
         String dbUser = "spring";
         String password = "book";
-        Connection con = DriverManager.getConnection(url, dbUser, password);
+        return DriverManager.getConnection(url, dbUser, password);
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection con = getConnection("jdbc:mysql://localhost:3306/springbook?serverTimezone=UTC&useSSL=false");
 
         String sql = "select * from users where id = ?";
         PreparedStatement ps = con.prepareStatement(sql);
@@ -54,7 +53,7 @@ public class UserDao {
         return user;
     }
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException { //main 메서드 :
         UserDao dao = new UserDao();
 
         User user = new User();
