4bb0699c7f9ef31c3025ff0fa27d4c44c7aad396
KBSure <kbsure.dev@gmail.com>
2018-06-22 14:33:54 +0900

    리스트 1.5

---
+++ src/main/java/springbook/user/dao/NUserDao.java
@@ -0,0 +1,19 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class NUserDao extends UserDao {
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+    
+        String url = "jdbc:mysql://localhost:3306/springbook?serverTimezone=UTC&useSSL=false";
+        String dbUser = "spring";
+        String password = "book";
+        Connection con = DriverManager.getConnection(url, dbUser, password);
+    
+        return con;
+    }
+}
--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -4,14 +4,14 @@ import springbook.user.domain.User;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        Connection con = getConnection("jdbc:mysql://localhost:3306/springbook");
+        Connection con = getConnection();
 
         String sql = "insert into users(id, name, password) values(?, ?, ?)";
-        PreparedStatement ps = con.prepareStatement(sql); //벤더사가 구현  //PS는 ? 사용 해서 동적 sql 가능 //sqlInjection 공격에 안전
+        PreparedStatement ps = con.prepareStatement(sql);
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
         ps.setString(3, user.getPassword());
@@ -22,17 +22,9 @@ public class UserDao {
         con.close();
     }
 
-    private Connection getConnection(String s) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver"); //각 DB 만든 회사들은 다르다. 패키지명도 다 다를테니까 호환성
-
-        String url = s;
-        String dbUser = "spring";
-        String password = "book";
-        return DriverManager.getConnection(url, dbUser, password);
-    }
-
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection con = getConnection("jdbc:mysql://localhost:3306/springbook?serverTimezone=UTC&useSSL=false");
+
+        Connection con = getConnection();
 
         String sql = "select * from users where id = ?";
         PreparedStatement ps = con.prepareStatement(sql);
@@ -53,8 +45,10 @@ public class UserDao {
         return user;
     }
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException { //main 메서드 :
-        UserDao dao = new UserDao();
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new NUserDao();
 
         User user = new User();
         user.setId("whiteship");
