60f99eba8f49b31b2e0815eb49dedb2387d58402
KBSure <kbsure.dev@gmail.com>
2018-06-26 23:25:08 +0900

    리스트 1-21

--- src/main/java/springbook/user/dao/SingletonTest.java
+++ src/main/java/springbook/user/dao/SingletonTest.java
@@ -1,17 +1,26 @@
 package springbook.user.dao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SingletonTest {
-    
+
     public static void main(String[] args) {
         DaoFactory daoFactory = new DaoFactory();
         UserDao dao1 = daoFactory.userDao();
         UserDao dao2 = daoFactory.userDao();
-    
+
         System.out.println(dao1);
         System.out.println(dao2);
-        System.out.println("dao1 == dao2" + (dao1 == dao2));
-        
-        
+        System.out.println("dao1 == dao2 : " + (dao1 == dao2));
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao3 = context.getBean("userDao", UserDao.class);
+        UserDao dao4 = context.getBean("userDao", UserDao.class);
+
+        System.out.println(dao3);
+        System.out.println(dao4);
+        System.out.println("dao3 == dao4 : "+ (dao3 == dao4));
     }
-    
+
 }
