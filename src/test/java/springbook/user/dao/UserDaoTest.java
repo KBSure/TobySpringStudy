15e0a559b9cc1bf11ecec98823016486aff53aa5
KBSure <kbsure.dev@gmail.com>
2018-07-04 21:51:07 +0900

    리스트 2-11 (p.169)

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -17,16 +17,28 @@ public class UserDaoTest {
         JUnitCore.main("springbook.user.dao.UserDaoTest");
     }
 
-    //getCount()와 deleteAll() 점검
-//    @Test
-//    public void getCount() throws SQLException {
-//        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-//        UserDao dao = context.getBean("userDao",UserDao.class);
-////        dao.deleteAll();
-//        assertThat(dao.getCount()).isEqualTo(0);
-//        addAndGet();
-//        assertThat(dao.getCount()).isEqualTo(1);
-//    }
+    @Test
+    public void count() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
+        //User필요
+        User user1 = new User("kbs", "김병수", "7");
+        User user2 = new User("mbc", "마봉춘", "11");
+        User user3 = new User("sbs", "신봉선", "5");
+
+        dao.add(user1);
+        assertThat(dao.getCount()).isEqualTo(1);
+
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
+
+        dao.add(user3);
+        assertThat(dao.getCount()).isEqualTo(3);
+    }
 
 
     @Test
