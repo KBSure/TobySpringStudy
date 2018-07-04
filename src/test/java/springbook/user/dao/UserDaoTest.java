6068935e597de25f4ad77435c587b2cad9f1b44d
KBSure <kbsure.dev@gmail.com>
2018-07-04 21:37:57 +0900

    리스트 2-9 (p.166)

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -3,6 +3,7 @@ package springbook.user.dao;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
@@ -16,18 +17,35 @@ public class UserDaoTest {
         JUnitCore.main("springbook.user.dao.UserDaoTest");
     }
 
+    //getCount()와 deleteAll() 점검
+//    @Test
+//    public void getCount() throws SQLException {
+//        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//        UserDao dao = context.getBean("userDao",UserDao.class);
+////        dao.deleteAll();
+//        assertThat(dao.getCount()).isEqualTo(0);
+//        addAndGet();
+//        assertThat(dao.getCount()).isEqualTo(1);
+//    }
+
+
     @Test
     public void addAndGet() throws SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 //        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
         UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
         User user = new User();
         user.setId("gyumee");
         user.setName("박성철");
         user.setPassword("springno1");
 
         dao.add(user);
+        assertThat(dao.getCount()).isEqualTo(1);
 
         User user2 = dao.get(user.getId());
 
